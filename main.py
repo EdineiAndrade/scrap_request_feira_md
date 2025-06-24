1056a4c71b9bf65bc1dd7a702b3a5317631b2be2
EdineiAndrade <edinei22.ba@gmail.com>
2025-06-24 00:07:20 -0300

    dfs img, tamanho, cores

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+import pandas as pd
 
 def scrap_feira_md():
     url_base = "https://www.feiradamadrugadasp.com.br"
@@ -54,7 +55,7 @@ def scrap_feira_md():
                 url_product = f"{url_base}{link_product}"
                 url_product = "https://www.feiradamadrugadasp.com.br/camiseta-masculina-basica-lisa-sem-estampa-plus-size/p/220744/"
                 item = soup.select_one('.item-product')
-                data_id = item.get('data-id') if item and item.has_attr('data-id') else 'N/A'
+                codigo_sku = item.get('data-id') if item and item.has_attr('data-id') else 'N/A'
                 response = requests.get(url_product)
 
                 if response.status_code != 200:
@@ -62,9 +63,11 @@ def scrap_feira_md():
                     continue
                 
                 product = BeautifulSoup(response.content, 'html.parser') 
-                name = product.find('h1', class_='name').text.strip()
-                price = float(product.find('strong', class_='sale_price').text.strip().replace('R$ ','').replace('.','').replace(',','.'))
-                cores = product.select('.cor .img img')
+                produto = product.find('h1', class_='name').text.strip()
+                codigo = url_product.split('/')[-2]
+                preco_custo = float(product.find('strong', class_='sale_price').text.strip().replace('R$ ','').replace('.','').replace(',','.'))
+                preco_venda = round(float(preco_custo)* 1.1,2)
+                categoria = url_categoria.split('/')[3].replace('-',' ').title()
                 lista_cores = [label.get_text(strip=True) for label in product.select('.cor .values .value:not(.disabled)')]
                 lista_tamanhos = [label.get_text(strip=True) for label in product.select('.tam .values span')]
                 
@@ -78,7 +81,7 @@ def scrap_feira_md():
                     for p in product.select('div[itemprop="description"] li')  # Seletor CSS equivalente
                     if p.get_text(strip=True)  # Ignora parágrafos vazios
                 ]
-                descricao = ' '.join(paragrafos_1 + paragrafos_2)
+                description = ' '.join(paragrafos_1 + paragrafos_2)
                 peso_produto = paragrafos_2[-2].replace('Peso do Produto:','')
                 codigo_upc = paragrafos_2[-1].replace('Código UPC:','') 
 
@@ -89,15 +92,158 @@ def scrap_feira_md():
                     "https:" + link.get('big_img') if link.get('big_img') else ""
                     for link in imagens
                 ]
-
                 # Remove duplicatas e strings vazias
                 lista_sem_duplicatas = list({img for img in lista_imagens if img})
-
                 # Junta com vírgula e espaço
                 lista_imagens_final = ", ".join(lista_sem_duplicatas) 
-                print(f"Product ID: {data_id}, Name: {name}, Price: {price}, Colors: {lista_cores}")
+                if lista_cores:
+                    df_cores = pd.DataFrame(lista_cores)
+                    cores_str = '; '.join(lista_cores)
+                    cor = 'Cor'
+                    cor_padrao = lista_cores[0] if lista_cores else ''
+                else:
+                    cor_padrao = cor = cores_str = df_cores = ""
+                    
+                if lista_tamanhos:
+                    df_tamanhos = pd.DataFrame(lista_tamanhos)
+                    tamanhos_str = '; '.join(lista_tamanhos)
+                    tamanho_meio = lista_tamanhos[0] if lista_tamanhos else ''
+                    atributo1 = 'Tamanho'
+                    atributo_global_2 = 1
+                else:
+                    tamanho_meio = atributo1 = tamanhos_str = df_tamanhos = ""
+                    atributo_global_2 = 0
+                # Cria o DataFrame imagem
+                if len(lista_sem_duplicatas) >0:
+                    df_imagens = pd.DataFrame(lista_sem_duplicatas)
+                    df_imagens = df_imagens.transpose()
+                    df_imagens.columns = [f'Imagem {i+1}' for i in range(len(df_imagens.columns))]
+                else:
+                    df_imagens =""
+                
+                return [df_tamanhos,df_cores,df_imagens,{
+                        'ID': codigo,
+                        'SKU': codigo_sku,
+                        'Código do Produto': codigo_upc,
+                        'Preço promocional': 0,
+                        "Tipo": "variable",
+                        "GTIN UPC EAN ISBN": "",
+                        'Nome': produto,
+                        "Publicado": 1,
+                        "Em Destaque": 0,
+                        "Visibilidade no Catálogo": "visible",
+                        "Descrição Curta": "",
+                        "Descrição": description,
+                        "Data de Preço Promocional Começa em": "",
+                        "Data de Preço Promocional Termina em": "",
+                        "Status do Imposto": "taxable",
+                        "Classe de Imposto": "parent",
+                        "Em Estoque": 0,
+                        "Estoque": "",
+                        "Quantidade Baixa de Estoque": 3,
+                        "São Permitidas Encomendas": 0,
+                        "Vendido Individualmente": 0,
+                        "Peso (kg)": peso_produto,
+                        "Comprimento (cm)": 32,
+                        "Largura (cm)": 20,
+                        "Altura (cm)": 12,
+                        "Permitir avaliações de clientes?": 1,
+                        "Nota de Compra": "",
+                        "Preço Promocional": "",
+                        "Preço de Custo": preco_custo,
+                        "Preço de Venda": preco_venda,
+                        "Categorias": categoria,
+                        "Tags": "",
+                        "Classe de Entrega": "",
+                        "Imagens": lista_imagens,
+                        "Limite de Downloads": "",
+                        "Dias para Expirar o Download": "",
+                        "Ascendente": f"id:{codigo}",
+                        "Grupo de Produtos": "",
+                        "Upsells": "",
+                        "Venda Cruzada": "",
+                        "URL Externa": "",
+                        "Texto do Botão": "",
+                        "Posição": 0,
+                        "Brands": "",
+                        "Nome do Atributo 1": atributo1,
+                        "Valores do Atributo 1": tamanhos_str,
+                        "Visibilidade do Atributo 1": 0,
+                        "Atributo Global 1": atributo1,
+                        "Atributo Padrão 1": tamanho_meio,
+                        "Nome do Atributo 2": cor,
+                        "Valores do Atributo 2": cores_str,
+                        "Visibilidade do Atributo 2": 0,
+                        "Atributo Global 2": atributo_global_2,
+                        "Atributo Padrão 2": cor_padrao,
+                        "Galpão": " Galpão 8"
+                    }]
+                
 
             #return product_list
+def process_products(products):
+    df_tamanhos, df_cores, df_imagens, produto_dict = products
+    try:
+                        # Otimização: Evitar múltiplos concat e explode desnecessários
+        df_produto = pd.DataFrame([produto_dict])
+        if len(df_imagens) > 0:
+            df_produto = pd.concat([df_produto, df_imagens], axis=1)
+
+        if len(df_cores) <= 1 and len(df_tamanhos) > 1:
+            coluna_atributo = "Valores do Atributo 1"
+            df_explodido = df_produto.explode(coluna_atributo).reset_index(drop=True)
+            df_explodido[coluna_atributo] = df_explodido[coluna_atributo].apply(lambda x: x.split(", ") if x == "P, M, G, GG" else ast.literal_eval(x) if isinstance(x, str) else x)
+            df_explodido = df_explodido.explode(coluna_atributo).reset_index(drop=True)
+        elif len(df_cores) <= 1 and len(df_tamanhos) <= 1:
+            df_explodido = df_produto.explode("Valores do Atributo 1").reset_index(drop=True)
+        else:
+            coluna_atributo = "Valores do Atributo 2"
+            df_explodido = df_produto.explode(coluna_atributo).reset_index(drop=True)
+            df_explodido[coluna_atributo] = df_explodido[coluna_atributo].str.split(", ")
+            df_explodido = df_explodido.explode(coluna_atributo, ignore_index=True)
+
+        df_explodido = df_explodido.drop(columns=["Estoque"])
+        if len(df_imagens) > 0:
+            numero_imagens = df_imagens.shape[1]
+            df_explodido.iloc[:, -numero_imagens:] = ""
+
+        df_explodido["Tipo"] = "variation"
+        df_explodido[["Descrição", "Peso (kg)", "Comprimento (cm)", "Largura (cm)", "Altura (cm)", "Categorias", "Visibilidade do Atributo 1", "Atributo Padrão 1", "Quantidade Baixa de Estoque", "Imagens"]] = None
+        df_produto[["Classe de Imposto", "Ascendente", "Preço de Custo", "Preço de Venda"]] = None
+        df_explodido["Permitir avaliações de clientes?"] = 0
+        df_explodido["Posição"] = df_explodido.index + 1
+        df_explodido["ID"] = df_explodido["ID"].astype(str) + df_explodido["Posição"].astype(str)
+
+        if len(df_cores) <= 1 and len(df_tamanhos) <= 1:
+            df_final = df_produto
+            pd.set_option('future.no_silent_downcasting', True)
+            df_final = df_final.fillna("").astype(str)
+            df_final["Tipo"] = "simple"
+        else:
+            df_ref = df_tamanhos if coluna_atributo == "Valores do Atributo 1" else df_cores
+            df_final = df_explodido.merge(df_ref[[coluna_atributo, "Estoque"]], on=coluna_atributo, how="left")
+            pd.set_option('future.no_silent_downcasting', True)
+            df_final = df_final.fillna("").astype(str)
+            df_final["Em Estoque"] = df_final["Estoque"]
+            df_final["Tipo"] = "variation"
+            df_final = pd.concat([df_produto, df_final], ignore_index=True)
+
+        products_data.append(df_final)
+        df_final = pd.concat(products_data, ignore_index=True)
+        df_final = df_final.fillna("")
+        cont += 1
+        if cont >= 200:
+            save_to_sheets(df_final)
+            cont = 0
+    except Exception as e:
+        print(f"Erro ao processar o produto {url_product}: {e}")
+        continue
+    
+    # Exemplo de como acessar as informações do produto
+    print("Informações do produto:")
+    for key, value in product_info.items():
+        print(f"{key}: {value}")
 if __name__ == "__main__":
     products = scrap_feira_md()
-    
+    process_products(products)
+    print("Scraping completed successfully.")
